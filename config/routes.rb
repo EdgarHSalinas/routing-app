b470c79c081dcbf7d1d884334d001ab2295aef82
Edgar <edgar.h.salinas@gmail.com>
2018-07-16 21:51:20 -0700

    Implemented nested routes of Admin/dashboard/ main user blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  
+  namespace :admin do
+    get 'dashboard/main'
+    get 'dashboard/user'
+    get 'dashboard/blog'
+  end
+
   get 'about', to: 'pages#about'
 
-  get 'contact', to: 'pages#contact'
+  get 'leadgeneration/example/lead', to: 'pages#contact', as: 'lead'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
 end
